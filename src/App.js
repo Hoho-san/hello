be2dccd3d28db40140d49351c49a6c9dd17524be
Hoho-san <javierjojo24@gmail.com>
2022-12-02 15:52:43 +0800

    1st hour

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
 import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
-    console.log("we are about to list the employees");
+    const [role, setRole] = useState("Junior Dev");
     const showEmployees = true;
     return (
         <div className="App">
             {showEmployees ? (
                 <>
-                    {" "}
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                            console.log(e.target.value);
+                            setRole(e.target.value);
+                        }}
+                    ></input>
                     <Employee name="Jojo" role="Intern" />
                     <Employee name="Power" role="Intern" />
                     <Employee name="Makima" role="SeniorDev" />
-                    <Employee name="Denji" />
+                    <Employee name="Denji" role={role} />
                     <Employee role="Intern" />
                 </>
             ) : (
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,15 +1,23 @@
 function Employee(props) {
+    const showEmployees = true;
+
     return (
-        <>
-            {/* you can choose in one of these */}
-            <h3>Employee: {props.name ? props.name : "No name"}</h3>
-            <p>{props.role ? props.role : "No role"}</p>
-            {props.role ? (
-                <p class="role">{props.role}</p>
+        <div className="App">
+            {showEmployees ? (
+                <>
+                    {/* you can choose in one of these */}
+                    <h3>Employee: {props.name ? props.name : "No name"}</h3>
+                    <p>{props.role ? props.role : "No role"}</p>
+                    {props.role ? (
+                        <p class="role">{props.role}</p>
+                    ) : (
+                        <p class="norole">No Role</p>
+                    )}
+                </>
             ) : (
-                <p class="norole">No Role</p>
+                <p>You cannot see employees</p>
             )}
-        </>
+        </div>
     );
 }
 export default Employee;
