51696bd6fc3140286a9b30bfca08c8b39027da49
Hoho-san <javierjojo24@gmail.com>
2022-12-02 10:47:05 +0800

    clean template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Employee from "./components/Employee";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
+                <Employee />
                 <p>Hello hahaha</p>
                 <a
                     className="App-link"
