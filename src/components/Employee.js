93b38a2a248709d0c523d57f4796b1ffba9d4e31
Hoho-san <javierjojo24@gmail.com>
2022-12-04 11:23:50 +0800

    Create Popup modal

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,3 +1,5 @@
+import EditEmployee from "./EditEmployee";
+
 function Employee(props) {
     // const showEmployees = true;
     return (
@@ -16,9 +18,8 @@ function Employee(props) {
                         {props.role ? props.role : "No role"}
                     </p>
                 </div>
-                <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                    Update
-                </button>
+
+                <EditEmployee />
             </div>
         </div>
     );
